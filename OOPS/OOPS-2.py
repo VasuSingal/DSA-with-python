264061fabeacfd70f54e948e93952261304ccade
Vasu Singal <97177511+VasuSingal@users.noreply.github.com>
2023-07-09 20:51:07 +0530

    Update OOPS-2.py

--- OOPS/OOPS-2.py
+++ OOPS/OOPS-2.py
@@ -36,3 +36,43 @@ class Circle(object):                                                       # sa
     
 c = Circle(3)
 print(c)
+
+#---------------------------------------------
+
+#Multiple Inheritance
+
+class Mother:
+    def print(self):
+        print ("Mother")
+    
+class Father:                                       # Since print is a function in both the base classes if the print function is called by the derived class
+    def print(self):                                #, print function of the class written first in the bracket of the derived class will be used
+        print("father")
+    
+class Child(Father, Mother):
+    def printChild(self):
+        print ("Child")
+    
+c = Child()
+print(Child.mro())                                  #to see the order in which the initailization will happen
+c.print()
+
+#---------------------------------------------
+
+#Operator Overloading
+
+class Point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def __add__(self,otherPoint):
+        return (self.x+otherPoint.x, self.y+otherPoint.y)
+    
+    def __lt__(self,otherPoint):                                #less than function compared using the hypotenuse 
+        return (math.sqrt(self.__x**2 + self.__y**2) < math.sqrt(point_object.__x**2 + point_object.__y**2))
+    
+p1 = Point(1,2)
+p2 = Point(4,5)
+
+print(p1+p2)
